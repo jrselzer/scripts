7259ba2b68bc9b9f22528b76281683eef90bb81a
jrselzer <js@crypto.koeln>
2021-10-17 13:09:45 +0200

    Use this tool to pull an Owncloud from a remote machine and adapt it to your local settings.

---
+++ owncloudbackup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+# Create a running Owncloud clone 
+AUTHOR=jrselzer@github.com
+VERSION="0.1 20211016"
+MYNAME=`basename $0 | cut -d. -f1`
+MYCONF=${MYNAME}.conf
+if [ -f ${MYCONF} ]
+then
+	. ${MYCONF} 
+else
+	echo "Configuration file ${MYCONF} not found, exiting."
+	exit 100
+fi
+TIMESTAMP=`date +%Y%m%d-%H%M%S`
+DBBACKUP=${MYNAME}_${TIMESTAMP}.dmp
+
+cat <<- EOF
+	Creating database backup. Enter remote shell user password and then 
+	database password
+EOF
+
+ssh ${RUSER}@${RHOST} mysqldump -u ${RDBUSER} -p ${RDBNAME} > ${DBBACKUP}
+
+echo "Syncing installation. Enter remote shell user password"
+
+rsync -av ${RUSER}@${RHOST}:${ROCDIR} ${WWWDIR}/
+chmod 755 ${OCDIR}
+chmod 644 ${OCDIR}/.htaccess
+
+echo "Replacing datafile paths in ${DBBACKUP}"
+
+ROCDATA=`grep datadirectory ${OCCONF} | cut -d"'" -f4`
+sed -i 's?'${ROCDATA}'?'${OCDATA}'?g' ${DBBACKUP}
+
+echo "Importing ${DBBACKUP}"
+
+mysql --login-path=ocbackup ${DBNAME} < ${DBBACKUP}
+
+echo "Adapting config"
+
+OCDATA=${OCDATA//\//\\/}
+
+sed -i "s/\('datadirectory' => '\).*/\1${OCDATA}',/; \
+        s/\('dbname' => '\).*'/\1${DBNAME}'/; \
+        s/\('dbuser' => '\).*'/\1${DBUSER}'/; \
+        s/\('dbpassword' => '\).*'/\1${DBPASS}'/; \
+        s/\(0 => '\).*'/\1${DOMAIN}'/" \
+        ${OCCONF}
