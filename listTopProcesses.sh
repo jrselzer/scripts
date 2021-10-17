bd5421f29f68955b87ff642302c8db1b6987ea89
jrselzer <js@crypto.koeln>
2021-10-17 21:48:49 +0200

    CPU usage threshold added

--- listTopProcesses.sh
+++ listTopProcesses.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Print top 10 most CPU consuming processes as CSV - if they consume at all
-VERSION="0.1 20211017"
+VERSION="0.3 20211017"
 AUTHOR=jrselzer@github.com
-
+THRESHOLD=3
 TS="`date \"+%Y-%m-%d %H:%M:%S\"`"
 
 ps aux | \
 	sort -nrk 3,3 | \
 	tail -n +2 | \
 	head -10 | \
-	while read -a a
+	while read -ra a
 	do 
 		CPUP=${a[2]}
-		if [ "${PCUP}" != "%CPU" ] && [ "${CPUP}" != "0.0" ]
+		if [ "${PCUP}" != "%CPU" ] && [ "${CPUP}" != "0.0" ] && [ `echo "${CPUP} / 1" | bc` -ge ${THRESHOLD} ]
 		then
-			L=$(($L +1))
+			L=$((L +1))
 			echo "\"${TS}\",\"${L}\",\"USR\",\"${a[0]}\",\"CPUP\",\"${CPUP}\",\"MEMP\",\"${a[3]}\",\"CMD\",\"${a[@]:10}\""
 		fi
 	done
