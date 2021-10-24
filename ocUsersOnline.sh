52b34f8c5b66c510c7612cf6ff439d66cdac3971
jrselzer <jrselzer@users.noreply.github.com>
2021-10-24 13:50:29 +0200

    Create ocUsersOnline.sh
    Count users online since $n minutes ago.

---
+++ ocUsersOnline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+INTERVAL="now -${1:-1} min"
+SINCE=`date -d "${INTERVAL}" +%s`
+OCDIR=/var/www/vhosts/wordpress/htdocs/owncloud
+
+cd ${OCDIR}
+php occ user:list -v | \
+	grep lastLogin | \
+	cut -d: -f2 | \
+	while read LASTLOGIN
+	do
+		if [ ${LASTLOGIN} -ge ${SINCE} ]
+		then
+			echo ${LASTLOGIN}
+		fi
+	done | \
+	wc -l
