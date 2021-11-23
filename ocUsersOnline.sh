54fa67c21bba2d6951537a9903a4f89a2e9e620c
jrselzer <jrselzer@users.noreply.github.com>
2021-11-23 21:45:06 +0100

    Update ocUsersOnline.sh
    Adapted to Nextloud

--- ocUsersOnline.sh
+++ ocUsersOnline.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 INTERVAL="now -${1:-1} min"
-SINCE=`date -d "${INTERVAL}" +%s`
+SINCE=`date -d "${INTERVAL}" "+%s"`
 OCDIR=/var/www/vhosts/wordpress/htdocs/owncloud
 
 cd ${OCDIR}
-/usr/local/bin/php occ user:list -v | \
-	grep lastLogin | \
-	cut -d: -f2 | \
+/usr/local/bin/php occ user:list -i | \
+	grep last_seen | \
+	cut -d: -f2- | \
 	while read LASTLOGIN
 	do
+		LASTLOGIN=`date -d "${LASTLOGIN}" "+%s"`
+
 		if [ ${LASTLOGIN} -ge ${SINCE} ]
 		then
 			echo ${LASTLOGIN}
